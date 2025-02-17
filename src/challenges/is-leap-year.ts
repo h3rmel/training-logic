a17de8889fb0be0d481da9f0d91ca06d6c1ea025
H3rmel <isaachermel@gmail.com>
2025-02-17 00:02:40 -0300

    docs: add docs to second degree equation

--- src/challenges/is-leap-year.ts
+++ src/challenges/is-leap-year.ts
@@ -18,5 +18,6 @@ export function isLeapYear(year: number): boolean {
   if (year % 100 === 0) {
     return false;
   }
+  
   return year % 4 === 0;
 }
--- src/challenges/second-degree-equation.ts
+++ src/challenges/second-degree-equation.ts
@@ -1,9 +1,22 @@
+/**
+ * Solves a second degree equation
+ *
+ * If the discriminant is negative, there is no solution.
+ *
+ * @param a The coefficient of x²
+ * @param b The coefficient of x
+ * @param c The constant term
+ * @returns The solutions of the equation
+ */
 export function secondDegreeEquation(a: number, b: number, c: number) {
   const delta = b ** 2 - 4 * a * c;
+
   if (delta < 0) {
     return "No solution";
   }
+
   const x1 = (-b + Math.sqrt(delta)) / (2 * a);
   const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
   return [x1, x2];
 }
