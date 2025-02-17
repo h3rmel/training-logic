b18f7eab9bf824ab8d97fab9c853dcc4754e05a4
H3rmel <isaachermel@gmail.com>
2025-02-17 00:01:07 -0300

    feat: add new logic solution, a second degree equation

--- src/challenges/second-degree-equation.ts
+++ src/challenges/second-degree-equation.ts
@@ -1,3 +1,9 @@
 export function secondDegreeEquation(a: number, b: number, c: number) {
-  
+  const delta = b ** 2 - 4 * a * c;
+  if (delta < 0) {
+    return "No solution";
+  }
+  const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+  const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+  return [x1, x2];
 }
